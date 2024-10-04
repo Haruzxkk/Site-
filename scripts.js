6899e5caaeddff1fe37c67e9b5b9caeb93cb0fd3
Haruzxkk <squadjunin816@gmail.com>
2024-10-04 19:26:57 -0300

    scripts.js

--- scripts.js
+++
@@ -1,5 +0,0 @@
-// Função de compra, redireciona ao Telegram com o produto já selecionado
-function comprarProduto(produto) {
-    const telegramUrl = `https://t.me/seuUsuarioNoTelegram?text=Estou interessado no ${produto}`;
-    window.open(telegramUrl, '_blank');
-}
