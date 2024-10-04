c19c5db69aa829e3328ed7c4efe69105b974015a
Haruzxkk <squadjunin816@gmail.com>
2024-10-04 19:13:03 -0300

    Site
    Site

--- scripts.js
+++ scripts.js
@@ -1,39 +1,3 @@
-// Inicializando as partículas em todo o site
-particlesJS('particles-js', {
-    particles: {
-        number: { value: 100 },
-        color: { value: '#800080' },
-        shape: {
-            type: 'circle',
-            stroke: { width: 0 },
-        },
-        opacity: { value: 0.5 },
-        size: { value: 3 },
-        line_linked: {
-            enable: true,
-            distance: 150,
-            color: '#800080',
-            opacity: 0.4,
-            width: 1,
-        },
-        move: {
-            enable: true,
-            speed: 2,
-            direction: 'none',
-            random: false,
-            straight: false,
-        },
-    },
-    interactivity: {
-        detect_on: 'canvas',
-        events: {
-            onhover: { enable: true, mode: 'repulse' },
-            onclick: { enable: true, mode: 'push' },
-        },
-    },
-    retina_detect: true,
-});
-
 // Função de compra, redireciona ao Telegram com o produto já selecionado
 function comprarProduto(produto) {
     const telegramUrl = `https://t.me/Devharuzx?text=Estou interessado no ${produto}`;
